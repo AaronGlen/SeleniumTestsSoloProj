aa361f594114b7ef8c77dd0eae610f043ce368a2
AaronGlen <aaglendinning@gmail.com>
2019-09-23 17:05:50 +0100

    added switchingwebpages

--- SeleniumTestsSoloProj/src/test/java/com/qa/HeroesAndTeamsSeleniumTest.java
+++ SeleniumTestsSoloProj/src/test/java/com/qa/HeroesAndTeamsSeleniumTest.java
@@ -4,6 +4,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.Beta;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HeroesAndTeamsSeleniumTest {
@@ -13,7 +15,7 @@ public class HeroesAndTeamsSeleniumTest {
     public void setUp(){
         System.setProperty(
                 "webdriver.chrome.driver",
-                "C:\\Users\\aagle\\Documents\\soloproj\\SeleniumTestsSoloProj\\src\\test\\java\\resources\\chromedriver.exe");
+                "C:\\Users\\Admin\\Documents\\SoloProj\\seleniumTests\\SeleniumTestsSoloProj\\src\\test\\java\\resources\\chromedriver.exe");
         driver = new ChromeDriver();
     }
 
@@ -30,10 +32,13 @@ public class HeroesAndTeamsSeleniumTest {
     }
 
     @Test
-    public void searchingWebPage() throws InterruptedException{
+    public void switchingWebPage() throws InterruptedException{
         driver.manage().window().maximize();
         driver.get( "http://35.222.59.218/SoloProject/heros.html");
         Thread.sleep(2000);
+        WebElement showMessage = driver.findElement(By.xpath("/html/body/div/div[1]/button[2]"));
+        showMessage.click();
+        Thread.sleep(5000);
 
     }
 
